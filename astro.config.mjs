4180e7d404680921f1ce2fd8353086f98dec2fa2
James McLeod <james.mcleod@narvi.co.uk>
2026-06-01 23:31:56 +0100

    feat(framework): untrack .astro/, remove sitemap until site URL known

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   outDir: './dist',
-  integrations: [preact(), sitemap()],
+  integrations: [preact()],
 });
